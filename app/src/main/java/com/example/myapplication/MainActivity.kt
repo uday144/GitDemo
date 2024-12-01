766ee4572e5dfd132a7ce3bcbfd5b90d5eb57eb0
uday singh <>
2024-12-01 19:27:03 +1100

    Master 4 commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Hello Master 3 $name!",
+            text = "Hello Master 4 $name!",
             modifier = modifier
         )
     }
