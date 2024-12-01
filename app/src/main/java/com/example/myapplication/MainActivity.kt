fdd1dcff7d8de7821dc23e90f9276700bfc19bc4
uday singh <>
2024-12-01 19:54:21 +1100

    Master 7

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -35,7 +35,7 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Column {
         Text(
-            text = "Hello Master 6 $name!",
+            text = "Hello Master 7 $name!",
             modifier = modifier
         )
     }
